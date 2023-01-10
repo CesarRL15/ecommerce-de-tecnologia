3a5710971fba5dba685bd82b1d535b9f565476de
Cesar-Ramirez <3519110771@uth.edu.mx>
2023-01-10 12:26:57 -0600

    Agregando JavaScript a nuestro proyecto

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+//const edadUsuario = prompt("Cuantos años tienes?")
+//const edadUsuario2 = prompt("Cuantos años tiene el usuario 2");
+
+//alert ("edad" + edadUsuario + edadUsuario2);
+
+//const operacion = 20;
+//const resultado1 = 30;
+//const resultado = operacion - resultado1; 
+//alert ("cuenta de para el usuario" + resultado)  
+
+//CONCATENACION
+//const saludo = alert("Hola " + nombreProducto + ", Tienes" + tipoProducto + " " + "Estas nuevas novedades.");
+
+//INTERPOLACION O TRANSPOLACIÓN
+//const saludoTranspolacio = alert (`Hola ${nombreProducto}, tienes ${tipoProducto} estos nuevos productos!`)
+
